bbd3f445101b85f29fbce0286437cac03c3a6386
Damian Oguche <damianguch@yahoo.com>
2023-05-09 13:10:52 -0700

    6-print_comb3.py file modified

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for j in range(0, 9):
-    for k in range(i + 1, 10):
+    for k in range(j + 1, 10):
         if j == 8 and k == 9:
             print("{:d}{:d}".format(j, k))
         else:
