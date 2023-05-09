9a7eec469094d8a268a1338da9ff6974a4d1738c
Damian Oguche <damianguch@yahoo.com>
2023-05-09 12:56:23 -0700

    6-print_comb3.py file created

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n in range(0, 10):
+    for m in range((n+ 1), 10):
+        if (n is not 8) or (m is not 9):
+            print("{}{}, ".format(n, m), end="")
+        else:
+            print("{}{}".format(n, m))
