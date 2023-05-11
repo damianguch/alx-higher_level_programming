276b98679e87981d426a90d4dd3e8beccd255de0
Damian Oguche <damianguch@yahoo.com>
2023-05-11 09:37:12 -0700

    102-magic_calculation.py file created

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,27 +1,6 @@
-def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0)
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
-
-
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for n in range(4, 6):
-            c = add(c, n)
-        return c
-    else:
-        return sub(a, b)
-
-
 import magic_calculation_102
+
 
 def magic_calculation(a, b):
     c = 0
@@ -32,4 +11,3 @@ def magic_calculation(a, b):
     else:
         c = magic_calculation_102.sub(a, b)
     return c
-
