0dc9b707e547aa58270ab51e9b0b75b9c7ec1c7f
Damian Oguche <damianguch@yahoo.com>
2023-05-11 10:12:07 -0700

    5-variable_load.py file created

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,17 +1,5 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
-    # import the variable 'a' from the file 'variable_load_5.py'
     from variable_load_5 import a
 
-    # print the value of 'a'
     print(a)
-
-#!/usr/bin/python3
-from variable_load_5 import a
-
-
-def find_var():
-    print(a)
-
-
-if __name__ == "__main__":
-    find_var()
