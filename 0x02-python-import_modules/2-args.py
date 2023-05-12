f595e0b23fa762ccc1b4022246e85fc2725623e6
Damian Oguche <damianguch@yahoo.com>
2023-05-12 00:45:06 -0700

    2-args.py file modified

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_arg(argv):
+if __name__ = "__main__":
+    from sys import argv
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
@@ -13,8 +14,3 @@ def print_arg(argv):
         while i <= n:
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
-
-
-if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
