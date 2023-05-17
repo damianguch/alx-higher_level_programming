3b7cfad479164cca8781c92b6dfe7adb98eeba66
Damian Oguche <damianguch@yahoo.com>
2023-05-17 11:41:42 -0700

    100-weight_average.py file modified

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,11 +2,11 @@
 def weight_average(my_list=[]):
     if my_list is not None:
         total = 0
-        frequency = 0
-        for tup in my_list:
-            (weight, occurence) = tup
-            total += (weight * occurence)
-            frequency += occurence
-        return (total/frequency) if frequency > 0 else 0
-    else:
-        return (0)
+        freq = 0
+        for t in my_list:
+            (score, weight) = t
+            total += (score * weight)
+            freq += weight
+        return (total/freq) if freq > 0 else 0
+    
+    return (0)
