8567fca9bbaf7d0fbb75cf7ec9b6940e189efaa9
Damian Oguche <damianguch@yahoo.com>
2023-05-17 09:47:17 -0700

    11-multiply_list_map.py file modified

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x * number, my_list)))
+    if my_list is not None:
+        my_list_cpy = my_list.copy()
+        return (list(map(lambda x: x * number, my_list_cpy)))
+    return None
