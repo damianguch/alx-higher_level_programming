48a63bdd1f88b21bab6e6d412755c56b98cfe1db
Damian Oguche <damianguch@yahoo.com>
2023-05-17 05:58:35 -0700

    2-uniq_add.py file created

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if my_list is not None:
+        sum_unique_int = 0
+
+        for x in set(my_list):
+            sum_unique_int = sum_unique_in + x
+
+        return (sum_unique_int)
+    return None
