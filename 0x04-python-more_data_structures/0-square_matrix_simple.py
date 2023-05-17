c27c75c6bd61761154038ad61f0f80db1e115341
Damian Oguche <damianguch@yahoo.com>
2023-05-17 04:42:55 -0700

    0-square_matrix_simple.py file modified

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for _ in matrix:
+    matrix_cpy = matrix.copy()
+    for _ in matrix_cpy:
         for i in _:
             print(i ** 2, end="")
         print()
