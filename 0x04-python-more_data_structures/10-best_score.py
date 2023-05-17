7fa4506df09445a5ba4cf99831d055b3c8d5aa32
Damian Oguche <damianguch@yahoo.com>
2023-05-17 09:06:32 -0700

    10-best_score.py file modified

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is not None:
-        max_key = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
-        return max_key
-
-    return None
+def best_score(my_dict):
+    if my_dict is None or my_dict == {}:
+        return None
+    biggest = max(my_dict.values())
+    for key, value in my_dict.items():
+        if value is biggest:
+            return key
