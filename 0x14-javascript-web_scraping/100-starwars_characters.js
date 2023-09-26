1527348e4cfa912e4ae4b37e0e86a6e7504dd087
Damian Oguche <damianguch@yahoo.com>
2023-09-26 07:49:04 -0700

    100-starwars_characters.js file modified

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
+
+let id = process.argv[2];
+let url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    for (let i in body.characters) {
+      request(body.characters[i], function (err, response, body) {
+	if (err) {
+	  console.log(err);
+	} else if (response.statusCode === 200) {
+	  console.log(JSON.parse(body).name);
+	}
       });
-    });
+    }
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
   }
 });
